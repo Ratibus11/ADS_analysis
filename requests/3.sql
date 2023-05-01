1e191450fa517c135f34da193d6fab669e25292e
Lucas Bastian <bastian.lucas@icloud.com>
2023-05-01 15:29:25 +0200

    Format requests

--- requests/3.sql
+++ requests/3.sql
@@ -1,25 +1,43 @@
-SELECT quantites_par_siecle_total.siecle as siecle, quantite_totale, quantite_aegypte, ROUND(quantite_aegypte::NUMERIC / quantite_totale::NUMERIC * 100, 2) AS part_aegypte
-
-FROM
-	(SELECT SUM(quantite) AS quantite_totale, siecle
-	FROM (
-		SELECT quantite, CEIL(annee / 100) + 1 AS siecle
-		FROM commande
-		LEFT JOIN province ON province.id = id_province)
-	AS quantites_par_commande_total
-	GROUP BY siecle)
-AS quantites_par_siecle_total
-
-LEFT JOIN
-
-	(SELECT SUM(quantite) AS quantite_aegypte, siecle
-	FROM (
-		SELECT quantite, CEIL(annee / 100) + 1 AS siecle
-		FROM commande
-		LEFT JOIN province ON province.id = id_province
-		WHERE province.nom = 'ÆGYPTE')
-	AS quantites_par_commande_aegypte
-	GROUP BY siecle)
-AS quantites_par_siecle_aegypte
-
-ON quantites_par_siecle_aegypte.siecle = quantites_par_siecle_total.siecle
+SELECT 
+  quantites_par_siecle_total.siecle as siecle, 
+  quantite_totale, 
+  quantite_aegypte, 
+  ROUND(
+    quantite_aegypte :: NUMERIC / quantite_totale :: NUMERIC * 100, 
+    2
+  ) AS part_aegypte 
+FROM 
+  (
+    SELECT 
+      SUM(quantite) AS quantite_totale, 
+      siecle 
+    FROM 
+      (
+        SELECT 
+          quantite, 
+          CEIL(annee / 100) + 1 AS siecle 
+        FROM 
+          commande 
+          LEFT JOIN province ON province.id = id_province
+      ) AS quantites_par_commande_total 
+    GROUP BY 
+      siecle
+  ) AS quantites_par_siecle_total 
+  LEFT JOIN (
+    SELECT 
+      SUM(quantite) AS quantite_aegypte, 
+      siecle 
+    FROM 
+      (
+        SELECT 
+          quantite, 
+          CEIL(annee / 100) + 1 AS siecle 
+        FROM 
+          commande 
+          LEFT JOIN province ON province.id = id_province 
+        WHERE 
+          province.nom = 'ÆGYPTE'
+      ) AS quantites_par_commande_aegypte 
+    GROUP BY 
+      siecle
+  ) AS quantites_par_siecle_aegypte ON quantites_par_siecle_aegypte.siecle = quantites_par_siecle_total.siecle
--- requests/5.sql
+++ requests/5.sql
@@ -1,40 +1,85 @@
-WITH ventes_par_artisan_par_siecle AS (
-	SELECT id_artisan, SUM(quantite) AS quantite, CEIL(annee / 100) + 1 AS siecle
-	FROM realiser
-	LEFT JOIN commande ON commande.id = id_commande
-	GROUP BY id_artisan, siecle)
-	
+WITH ventes_par_artisan_par_siecle AS 
+(
+  SELECT
+    id_artisan,
+    SUM(quantite) AS quantite,
+    CEIL(annee / 100) + 1 AS siecle 
+  FROM
+    realiser 
+    LEFT JOIN
+      commande 
+      ON commande.id = id_commande 
+  GROUP BY
+    id_artisan,
+    siecle 
+)
 SELECT
-	siecle_1.siecle,
-	quantite_s1,
-	quantite_s2,
-	ROUND(((quantite_s2::NUMERIC / quantite_s1::NUMERIC) - 1) * 100, 2) AS diff_s1_s2,
-	quantite_s3,
-	ROUND(((quantite_s3::NUMERIC / quantite_s2::NUMERIC) - 1) * 100, 2) AS diff_s2_s3,
-	quantite_s4,
-	ROUND(((quantite_s4::NUMERIC / quantite_s3::NUMERIC) - 1) * 100, 2) AS diff_s3_s4,
-	quantite_s5,
-	ROUND(((quantite_s5::NUMERIC / quantite_s4::NUMERIC) - 1) * 100, 2) AS diff_s4_s5
-FROM (
-	SELECT id_artisan, quantite AS quantite_s1, siecle FROM ventes_par_artisan_par_siecle WHERE siecle = 1)
-AS siecle_1
-
-LEFT JOIN (
-	SELECT id_artisan, quantite AS quantite_s2 FROM ventes_par_artisan_par_siecle WHERE siecle = 2)
-AS siecle_2
-ON siecle_2.id_artisan = siecle_1.id_artisan
-
-LEFT JOIN (
-	SELECT id_artisan, quantite AS quantite_s3 FROM ventes_par_artisan_par_siecle WHERE siecle = 3)
-AS siecle_3
-ON siecle_3.id_artisan = siecle_1.id_artisan
-
-LEFT JOIN (
-	SELECT id_artisan, quantite AS quantite_s4 FROM ventes_par_artisan_par_siecle WHERE siecle = 4)
-AS siecle_4
-ON siecle_4.id_artisan = siecle_1.id_artisan
-
-LEFT JOIN (
-	SELECT id_artisan, quantite AS quantite_s5 FROM ventes_par_artisan_par_siecle WHERE siecle = 5)
-AS siecle_5
-ON siecle_5.id_artisan = siecle_1.id_artisan
+  quantite_s1,
+  quantite_s2,
+  ROUND( ( ( quantite_s2 :: NUMERIC / quantite_s1 :: NUMERIC ) - 1 ) * 100, 2 ) AS diff_s1_s2,
+  quantite_s3,
+  ROUND( ( ( quantite_s3 :: NUMERIC / quantite_s2 :: NUMERIC ) - 1 ) * 100, 2 ) AS diff_s2_s3,
+  quantite_s4,
+  ROUND( ( ( quantite_s4 :: NUMERIC / quantite_s3 :: NUMERIC ) - 1 ) * 100, 2 ) AS diff_s3_s4,
+  quantite_s5,
+  ROUND( ( ( quantite_s5 :: NUMERIC / quantite_s4 :: NUMERIC ) - 1 ) * 100, 2 ) AS diff_s4_s5 
+FROM
+  (
+    SELECT
+      id_artisan,
+      quantite AS quantite_s1,
+      siecle 
+    FROM
+      ventes_par_artisan_par_siecle 
+    WHERE
+      siecle = 1 
+  )
+  AS siecle_1 
+  LEFT JOIN
+    (
+      SELECT
+        id_artisan,
+        quantite AS quantite_s2 
+      FROM
+        ventes_par_artisan_par_siecle 
+      WHERE
+        siecle = 2 
+    )
+    AS siecle_2 
+    ON siecle_2.id_artisan = siecle_1.id_artisan 
+  LEFT JOIN
+    (
+      SELECT
+        id_artisan,
+        quantite AS quantite_s3 
+      FROM
+        ventes_par_artisan_par_siecle 
+      WHERE
+        siecle = 3 
+    )
+    AS siecle_3 
+    ON siecle_3.id_artisan = siecle_1.id_artisan 
+  LEFT JOIN
+    (
+      SELECT
+        id_artisan,
+        quantite AS quantite_s4 
+      FROM
+        ventes_par_artisan_par_siecle 
+      WHERE
+        siecle = 4 
+    )
+    AS siecle_4 
+    ON siecle_4.id_artisan = siecle_1.id_artisan 
+  LEFT JOIN
+    (
+      SELECT
+        id_artisan,
+        quantite AS quantite_s5 
+      FROM
+        ventes_par_artisan_par_siecle 
+      WHERE
+        siecle = 5 
+    )
+    AS siecle_5 
+    ON siecle_5.id_artisan = siecle_1.id_artisan
